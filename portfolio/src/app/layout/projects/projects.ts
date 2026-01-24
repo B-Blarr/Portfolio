6ff001b67389bd57b4a61ec1fe67578c145ee004
Benjamin Blarr <b.blarr@gmx.de>
2026-01-24 16:43:10 +0100

    Added Overlay to the projects when they are clicked

--- portfolio/src/app/layout/projects/projects.ts
+++ portfolio/src/app/layout/projects/projects.ts
@@ -1,11 +1,78 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { ProjectOverlay} from '../../shared/components/project-overlay/project-overlay';
+
+
+// @Component({
+//   selector: 'app-projects',
+//   imports: [ProjectOverlay],
+//   templateUrl: './projects.html',
+//   styleUrl: './projects.scss',
+// })
+// export class Projects {
+
+// }
+
+import { Component, } from '@angular/core';
+import { ProjectOverlay, ProjectData,} from '../../shared/components/project-overlay/project-overlay';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [ProjectOverlay, NgIf],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {
+// export class Projects {
+//   projects: ProjectData[] = [
+//     {
+//       title: 'Join',
+//       subtitle: 'Kanban Board',
+//       description: 'A task manager with auth and realtime features.',
+//       imageUrl: 'assets/img/projects/join.png',
+//       techs: [
+//         { name: 'Angular', iconUrl: 'assets/img/tech/angular.svg' },
+//         { name: 'TypeScript', iconUrl: 'assets/img/tech/typescript.svg' },
+//       ],
+//     },
+//     {
+//       title: 'Pokedex',
+//       subtitle: 'PokeAPI App',
+//       description: 'Search, filter and detail views powered by an API.',
+//       imageUrl: 'assets/img/projects/pokedex.png',
+//       techs: [
+//         { name: 'HTML', iconUrl: 'assets/img/tech/html.svg' },
+//         { name: 'CSS', iconUrl: 'assets/img/tech/css.svg' },
+//       ],
+//     },
+//   ];
 
+//   selectedProject: ProjectData = this.projects[0];
+// }
+
+export class Projects {
+  joinProject: ProjectData = {
+    title: 'Join',
+    subtitle: 'Kanban Board',
+    description: 'A task manager with drag & drop and user management.',
+    imageUrl: 'assets/img/projects/join.png',
+    techs: [
+      { name: 'Angular', iconUrl: 'assets/img/tech/angular.svg' },
+      { name: 'TypeScript', iconUrl: 'assets/img/tech/typescript.svg' },
+      { name: 'HTML', iconUrl: 'assets/img/tech/html.svg' },
+      { name: 'SCSS', iconUrl: 'assets/img/tech/scss.svg' },
+      { name: 'Supabase', iconUrl: 'assets/img/tech/supabase.svg' },
+    ],
+  };
+
+  selectedProject: ProjectData | null = null;
+  isOverlayOpen = false;
+
+  openJoinOverlay(): void {
+    this.selectedProject = this.joinProject;
+    this.isOverlayOpen = true;
+  }
+
+  closeOverlay(): void {
+    this.isOverlayOpen = false;
+  }
 }
--- portfolio/src/app/shared/components/project-overlay/project-overlay.ts
+++ portfolio/src/app/shared/components/project-overlay/project-overlay.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { AnimatedButton } from '../animated-button/animated-button';
+
+export type ProjectTech = {
+  iconUrl: string;
+  name: string;
+};
+
+export type ProjectData = {
+  title: string;
+  subtitle: string;
+  description: string;
+  imageUrl: string;
+  techs: ProjectTech[];
+};
 
 @Component({
   selector: 'app-project-overlay',
-  imports: [],
+  imports: [AnimatedButton, NgFor],
   templateUrl: './project-overlay.html',
   styleUrl: './project-overlay.scss',
 })
 export class ProjectOverlay {
-
+@Input({ required: true }) project!: ProjectData;
 }
