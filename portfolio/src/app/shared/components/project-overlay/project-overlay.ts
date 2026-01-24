66180b1cae24468465f949afc6826bf4c0692f0c
Benjamin Blarr <b.blarr@gmx.de>
2026-01-24 13:11:42 +0100

    Added projects section

---
+++ portfolio/src/app/shared/components/project-overlay/project-overlay.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-overlay',
+  imports: [],
+  templateUrl: './project-overlay.html',
+  styleUrl: './project-overlay.scss',
+})
+export class ProjectOverlay {
+
+}
--- portfolio/src/app/wrapper/main-wrapper/main-wrapper.ts
+++ portfolio/src/app/wrapper/main-wrapper/main-wrapper.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Aboutme } from "../../layout/aboutme/aboutme";
 import { Skills } from "../../layout/skills/skills";
+import { Projects } from "../../layout/projects/projects";
 
 @Component({
   selector: 'app-main-wrapper',
-  imports: [Aboutme, Skills],
+  imports: [Aboutme, Skills, Projects],
   templateUrl: './main-wrapper.html',
   styleUrl: './main-wrapper.scss',
 })
