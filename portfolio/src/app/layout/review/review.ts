d76b231ab18c796c24f247cf2b1cf75b1de53620
Benjamin Blarr <b.blarr@gmx.de>
2026-01-27 09:02:15 +0100

    Added reference and review components

---
+++ portfolio/src/app/layout/review/review.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Reference } from '../../shared/components/reference/reference';
+
+@Component({
+  selector: 'app-review',
+  imports: [Reference],
+  templateUrl: './review.html',
+  styleUrl: './review.scss',
+})
+export class Review {
+
+}
--- portfolio/src/app/wrapper/main-wrapper/main-wrapper.ts
+++ portfolio/src/app/wrapper/main-wrapper/main-wrapper.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Aboutme } from "../../layout/aboutme/aboutme";
 import { Skills } from "../../layout/skills/skills";
 import { Projects } from "../../layout/projects/projects";
+import { Review } from "../../layout/review/review";
 
 @Component({
   selector: 'app-main-wrapper',
-  imports: [Aboutme, Skills, Projects],
+  imports: [Aboutme, Skills, Projects, Review],
   templateUrl: './main-wrapper.html',
   styleUrl: './main-wrapper.scss',
 })
