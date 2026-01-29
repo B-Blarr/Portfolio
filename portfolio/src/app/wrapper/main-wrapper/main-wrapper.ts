645e0ed378b581bb0d13baaf8e9fe8d0625c4cdd
Benjamin Blarr <b.blarr@gmx.de>
2026-01-29 20:05:59 +0100

    Added footer component, styled footer component

--- portfolio/src/app/wrapper/main-wrapper/main-wrapper.ts
+++ portfolio/src/app/wrapper/main-wrapper/main-wrapper.ts
@@ -4,10 +4,11 @@ import { Skills } from "../../layout/skills/skills";
 import { Projects } from "../../layout/projects/projects";
 import { Review } from "../../layout/review/review";
 import { Contact } from "../../layout/contact/contact";
+import { Footer } from "../../layout/footer/footer";
 
 @Component({
   selector: 'app-main-wrapper',
-  imports: [Aboutme, Skills, Projects, Review, Contact],
+  imports: [Aboutme, Skills, Projects, Review, Contact, Footer],
   templateUrl: './main-wrapper.html',
   styleUrl: './main-wrapper.scss',
 })
