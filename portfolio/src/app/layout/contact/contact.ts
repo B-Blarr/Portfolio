d10fbbcb38ba318d05236aa5720640b80258b17c
Benjamin Blarr <b.blarr@gmx.de>
2026-02-01 13:19:23 +0100

    Changed responsiveness for header for smaller devices

--- portfolio/src/app/layout/contact/contact.ts
+++ portfolio/src/app/layout/contact/contact.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { Form } from '../../shared/components/form/form';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  imports: [Form, ReactiveFormsModule, TranslatePipe, RouterLink],
+  imports: [Form, ReactiveFormsModule, TranslatePipe],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
