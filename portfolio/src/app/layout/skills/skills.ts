ace30183fc2a0a1790547dd876ed00245e52217d
Benjamin Blarr <b.blarr@gmx.de>
2026-02-10 16:57:13 +0100

    Fixed a few responsiveness bugs

--- portfolio/src/app/layout/skills/skills.ts
+++ portfolio/src/app/layout/skills/skills.ts
@@ -3,7 +3,6 @@ import { AnimatedButton } from '../../shared/components/animated-button/animated
 import { TranslatePipe } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
 
-
 @Component({
   selector: 'app-skills',
   imports: [AnimatedButton, TranslatePipe, RouterLink],
